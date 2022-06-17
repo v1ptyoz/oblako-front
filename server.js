67db6cc7f082bc79caa4334ce15eb1bd3eb0194a
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-06-17 08:26:55 +0700

    add server for heroku deploy

---
+++ server.js
@@ -0,0 +1,20 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+const express = require('express');
+const app = express();
+
+app.use(morgan('combined'));
+app.use(requireHTTPS);
+
+app.use(express.static('./dist/oblako-front'));
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/oblako-front'});
+});
+
+app.listen(process.env.PORT || 8080);
