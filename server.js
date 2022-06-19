1934ad8bc54a1499230d78d65656809b7ba78834
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-06-19 11:22:18 +0700

    cors config

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const cors = require('cors');
+
 function requireHTTPS(req, res, next) {
   // The 'x-forwarded-proto' check is for Heroku
   if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
@@ -8,7 +10,21 @@ function requireHTTPS(req, res, next) {
 const express = require('express');
 const app = express();
 
+const corsOption = {
+  origin: function (origin, callback) {
+    if (!origin) {
+      return callback(null, true);
+    }
+
+    if (origin !== "https://khrabrov-oblako-front-2.herokuapp.com") {
+      return callback(new Error(`Origin is ${origin}`))
+    }
+    return callback(null, true);
+  }
+}
+
 app.use(requireHTTPS);
+app.use(cors(corsOption));
 
 app.use(express.static('./dist/oblako-front'));
 
